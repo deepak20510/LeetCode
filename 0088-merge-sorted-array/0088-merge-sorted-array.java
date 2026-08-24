06c5e4efc349b8743bce5ab761d865616c6be40c
Deepak Mahato <deepakmahato20510@gmail.com>
2026-08-24 16:49:12 +0530

    Time: 1 ms (26.52%), Space: 43.7 MB (78.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,32 +1,36 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        long[] nums3 = new long[n+m];
-        int left = 0;
-        int right = 0;
-        int index = 0;
-        while(left < m && right < n){
-            if(nums1[left] <= nums2[right]){
-                nums3[index] = nums1[left];
-                left++;
-                index++;
-            }else{
-                nums3[index] = nums2[right];
-                right++;
-                index++;
-            }
+    private void swapIfGreater(int[] nums1, int ind1, int[] nums2, int ind2){
+        if(nums1[ind1] > nums2[ind2]){
+            int temp = nums1[ind1];
+            nums1[ind1] = nums2[ind2];
+            nums2[ind2] = temp;
         }
-        while(left < m ){
-            nums3[index] = nums1[left];
-            index++;
-            left++;
-        }
-        while(right < n){
-            nums3[index] = nums2[right];
-            right++;
-            index++;
-        }
-        for(int i = 0; i < m + n; i++){
-            nums1[i] = (int) nums3[i];
     }
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len = (n + m);
+        int gap = (len / 2) + (len % 2);
+        while(gap > 0){
+            int left = 0;
+            int right = left + gap;
+            while(right < len){
+                //nums1 and nums2
+                if(left < m && right >= m){
+                    swapIfGreater(nums1, left,nums2, right - m);
+                }else if(left >= m){
+                    swapIfGreater(nums2,  left - m,nums2, right - m);
+                }else{
+                    swapIfGreater(nums1,  left,nums1, right);
+                }
+                left++;
+                right++;
+            }
+            if(gap == 1){
+                break;
+            }
+            gap = (gap / 2) + (gap % 2);
+        }
+        for(int i = 0; i < n; i++) {
+    nums1[m + i] = nums2[i];
 }
+    }
 }
