5e001a9bddbf6a59cce13ea3a5da3c8e9948c28b
Deepak Mahato <deepakmahato20510@gmail.com>
2026-08-25 22:49:36 +0530

    Time: 3 ms (72.87%), Space: 52.3 MB (75.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,49 +1,46 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        
+
         List<Integer> list = new ArrayList<>();
         int n = nums.length;
         int count1 = 0;
         int count2 = 0;
         int ele1 = 0;
         int ele2 = 0;
-        int n1= n/3;
-     
-        for(int i = 0; i < n;i++){
-            if(count1 == 0 && nums[i]!=ele2 ){
+        int n1 = n / 3;
+
+        for (int i = 0; i < n; i++) {
+            if (count1 == 0 && nums[i] != ele2) {
                 count1 = 1;
-                ele1 = nums[i];                        
-            }else if(count2 == 0 && nums[i]!=ele1 ){
+                ele1 = nums[i];
+            } else if (count2 == 0 && nums[i] != ele1) {
                 count2 = 1;
-                ele2 = nums[i]; 
-                
-                 }
-                 else if(nums[i] == ele1){
-                    count1++;
-                 }
-                 else if(nums[i] == ele2){
-                    count2++;
-                 }
-                else{
+                ele2 = nums[i];
+
+            } else if (nums[i] == ele1) {
+                count1++;
+            } else if (nums[i] == ele2) {
+                count2++;
+            } else {
                 count1--;
                 count2--;
             }
         }
-         count1=0;
-          count2 = 0;
-        for(int i = 0;i < n;i++){
-            if(nums[i] == ele1){
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == ele1) {
                 count1++;
-            }else if(nums[i] == ele2){
+            } else if (nums[i] == ele2) {
                 count2++;
             }
         }
-            if(count1 > n1 ){
-               list.add(ele1);
-            }
-            if(count2 > n1){
-                list.add(ele2);
-            }
+        if (count1 > n1) {
+            list.add(ele1);
+        }
+        if (count2 > n1) {
+            list.add(ele2);
+        }
         return list;
     }
 }
