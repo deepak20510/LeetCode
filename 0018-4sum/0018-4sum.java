2354a66742c75378b5ffc5b435a5db995540e9d8
Deepak Mahato <deepakmahato20510@gmail.com>
2026-08-26 21:07:54 +0530

    Time: 20 ms (66.31%), Space: 46 MB (21.69%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,22 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             for (int j = i + 1; j < n; j++) {
-                HashSet<Integer> seen = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    long required = (long) target - nums[i] - nums[j] - nums[k];
-                    if (required >= Integer.MIN_VALUE && required <= Integer.MAX_VALUE
-                            && seen.contains((int) required)) {
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], (int) required);
-                        Collections.sort(temp);
-                        set.add(temp);
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
                     }
-                    seen.add(nums[k]);
                 }
             }
         }
-        return new ArrayList<>(set);
+        return ans;
     }
 }
